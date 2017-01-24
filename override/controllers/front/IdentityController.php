38e94ab0f3a2a0731781e40ac393a8200c4a9215
Kubilay Erdogan <kublaios@gmail.com>
2017-01-24 19:17:38 +0200

    overiding IdentityController to catch subscription during profile update

---
+++ override/controllers/front/IdentityController.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * 2017 Thirty Bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    Thirty Bees <modules@thirtybees.com>
+ * @copyright 2017 Thirty Bees
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
+class IdentityController extends IdentityControllerCore
+{
+    public function postProcess()
+    {
+        $origin_newsletter = (bool)$this->customer->newsletter;
+
+        if (Tools::isSubmit('submitIdentity')) {
+            $email = trim(Tools::getValue('email'));
+
+            if (Tools::getValue('months') != '' && Tools::getValue('days') != '' && Tools::getValue('years') != '') {
+                $this->customer->birthday = (int)Tools::getValue('years').'-'.(int)Tools::getValue('months').'-'.(int)Tools::getValue('days');
+            } elseif (Tools::getValue('months') == '' && Tools::getValue('days') == '' && Tools::getValue('years') == '') {
+                $this->customer->birthday = null;
+            } else {
+                $this->errors[] = Tools::displayError('Invalid date of birth.');
+            }
+
+            if (Tools::getIsset('old_passwd')) {
+                $old_passwd = trim(Tools::getValue('old_passwd'));
+            }
+
+            if (!Validate::isEmail($email)) {
+                $this->errors[] = Tools::displayError('This email address is not valid');
+            } elseif ($this->customer->email != $email && Customer::customerExists($email, true)) {
+                $this->errors[] = Tools::displayError('An account using this email address has already been registered.');
+            } elseif (!Tools::getIsset('old_passwd') || (Tools::encrypt($old_passwd) != $this->context->cookie->passwd)) {
+                $this->errors[] = Tools::displayError('The password you entered is incorrect.');
+            } elseif (Tools::getValue('passwd') != Tools::getValue('confirmation')) {
+                $this->errors[] = Tools::displayError('The password and confirmation do not match.');
+            } else {
+                $prev_id_default_group = $this->customer->id_default_group;
+
+                // Merge all errors of this file and of the Object Model
+                $this->errors = array_merge($this->errors, $this->customer->validateController());
+            }
+
+            if (!count($this->errors)) {
+                $this->customer->id_default_group = (int)$prev_id_default_group;
+                $this->customer->firstname = Tools::ucwords($this->customer->firstname);
+
+                if (Configuration::get('PS_B2B_ENABLE')) {
+                    $this->customer->website = Tools::getValue('website'); // force update of website, even if box is empty, this allows user to remove the website
+                    $this->customer->company = Tools::getValue('company');
+                }
+
+                if (!Tools::getIsset('newsletter')) {
+                    $this->customer->newsletter = 0;
+                } elseif (!$origin_newsletter && Tools::getIsset('newsletter')) {
+                    if ($module_newsletter = Module::getInstanceByName('blocknewsletter')) {
+                        /** @var Blocknewsletter $module_newsletter */
+                        if ($module_newsletter->active) {
+                            $module_newsletter->confirmSubscription($this->customer->email);
+                        }
+                    }
+                }
+
+                if (!Tools::getIsset('optin')) {
+                    $this->customer->optin = 0;
+                }
+                if (Tools::getValue('passwd')) {
+                    $this->context->cookie->passwd = $this->customer->passwd;
+                }
+                if ($this->customer->update()) {
+                    $this->context->cookie->customer_lastname = $this->customer->lastname;
+                    $this->context->cookie->customer_firstname = $this->customer->firstname;
+                    $this->context->smarty->assign('confirmation', 1);
+                } else {
+                    $this->errors[] = Tools::displayError('The information cannot be updated.');
+                }
+            }
+        } else {
+            $_POST = array_map('stripslashes', $this->customer->getFields());
+        }
+
+        return $this->customer;
+    }
+}
